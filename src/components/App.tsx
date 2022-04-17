d1c32ec002354f1978977003469ab32c9586b1e7
= <=>
2022-04-17 15:40:03 +0200

    added auth service

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,14 @@
 import React from "react";
+import { User } from "../models/Models";
+import { AuthService } from "../services/AuthService";
+
+interface AppStat {
+  user: User | undefined;
+}
 
 export class App extends React.Component {
+  private authService: AuthService = new AuthService();
+
   render() {
     return <h2>Hello</h2>;
   }
---
+++ src/services/AuthService.ts
@@ -0,0 +1,12 @@
+import { User } from "../models/Models";
+
+export class AuthService {
+  public async login(
+    userName: string,
+    password: string
+  ): Promise<User | undefined> {
+    return userName === "user" && password === "1234"
+      ? { userName: userName, email: "sample@gmail.com" }
+      : undefined;
+  }
+}
