0d463d3c122bc80b46a1a7528c30d14e4fbc85d2
= <=>
2022-04-17 16:26:33 +0200

    added state model

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import { User } from "../model/Models";
+import { AuthService } from "../services/AuthService";
 
+interface AppStat {
+  user: User | undefined;
+}
 export class App extends React.Component {
   render() {
     return <h2>Hello</h2>;
--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,1 +1,12 @@
-export class AuthService {}
+import { User } from "../model/Models";
+
+export class AuthService {
+  public async login(
+    userName: string,
+    password: string
+  ): Promise<User | undefined> {
+    return userName === "user" && password === "1234"
+      ? { userName: userName, email: "sample@gmail.com" }
+      : undefined;
+  }
+}
